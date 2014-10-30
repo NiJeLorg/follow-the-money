fb29fb285d8da00bb283b6802a87383f89efae36
JD Godchaux <jd@nijel.org>
2014-10-30 16:57:40 -0400

    bug fixes

--- CashCity/static/CashCity/js/navbar.js
+++ CashCity/static/CashCity/js/navbar.js
@@ -6,11 +6,11 @@
 $( document ).ready(function() {
 	// for navbars, set active if clicked
 	$('.nav li').removeClass('active');
-    var url = window.location;
-    $('ul.nav a[href="'+ url +'"]').parent().addClass('active');
-    $('ul.nav a').filter(function() {
-         return this.href == url;
-    }).parent().addClass('active');
-		
+	// for trunk url select map, for all others select class
+	if (location.pathname.split("/")[1] == '') {
+		$('#navMapTab').addClass('active');
+	} else {
+		$('ul.nav a[href^="/' + location.pathname.split("/")[1] + '"]').parent().addClass('active');		
+	}
 });
 
