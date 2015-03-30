e88c0afa73b82c3157b0d5403a4df57498da3cf3
JD Godchaux <jd@nijel.org>
2015-03-30 18:16:38 -0400

    Updated convert video command to handle gp3 and mp4

--- CashCity/management/commands/convert_uploaded_video.py
+++ CashCity/management/commands/convert_uploaded_video.py
@@ -12,8 +12,8 @@ class Command(BaseCommand):
     args = ''
     help = ''
 
-    def convertMOVToMp3(self,(dirpath,filename)):
-        pathToMOV = os.path.join(dirpath,filename)
+    def convertVIDEOToMp3(self,(dirpath,filename)):
+        pathToVIDEO = os.path.join(dirpath,filename)
         pathToMP3 = os.path.join(dirpath,"%s.mp3" % (filename))
 
         print "Creating file: " + pathToMP3
@@ -21,26 +21,27 @@ class Command(BaseCommand):
         # ffmpeg -i .MOV test.mp3
         call(["ffmpeg","-i",pathToMOV,pathToMP3])
 
-    def findMOVFiles(self, dirpath,dirnames,filenames):
+    def findVIDEOFiles(self, dirpath,dirnames,filenames):
         fileToProcess = None
         alreadyEncodedFlag = False
         for filename in filenames:
-            if ".mov" in filename:
+            if ".mov" in filename or ".gp3" in filename or ".mp4" in filename:
                 fileToProcess = (dirpath,filename)
             if ".mp3" in filename:
                 alreadyEncodedFlag = True
         if (not alreadyEncodedFlag and fileToProcess is not None):
-             self.convertMOVToMp3(fileToProcess)
+             self.convertVIDEOToMp3(fileToProcess)
+
 
     def getDirectoryListing(self):
         """
             Returns a listing of the application's audio directory
         """
         for dirpath, dirnames, filenames in os.walk(os.path.dirname(os.path.realpath(__file__)) + "/../../../CityDigits/media/audio/"):
-            self.findMOVFiles(dirpath, dirnames, filenames)
+            self.findVIDEOFiles(dirpath, dirnames, filenames)
 
     def handle(self, *args, **options):
-        print "Starting conversion from MOV to MP3...."
+        print "Starting conversion from VIDEO to MP3...."
         self.getDirectoryListing()
         print "Conversion complete..."
 
